34962b5f34a3adb7f1dadc5dd5dedb67bbaa4441
Linxir <40857147+Linxir@users.noreply.github.com>
2024-10-10 11:26:23 +0800

    Update Wyyd.js

--- Script/Wyyd.js
+++ Script/Wyyd.js
@@ -6,7 +6,7 @@
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wyyd.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://github.com/Linxir/loon/raw/refs/heads/main/Script/Wyyd.js
 
 [mitm] 
 
